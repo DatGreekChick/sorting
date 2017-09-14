f4c3172612367c0a43b6bff2c4be9b5c230438eb
Eleni A <eleni.arvanitis@me.com>
2017-09-14 15:07:56 -0400

    final bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -1,18 +1,23 @@
-const swap = (val1, val2) => {
+function swap(val1, val2) {
   return [val2, val1];
-};
+}
 
 const bubbleSort = arr => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i + 1]) {
-      const result = swap(arr[i], arr[i + 1]);
+  debugger;
+  let hasSwapped = true;
 
-      // console.log('larger:',larger,'-','smaller:',smaller);
+  while (hasSwapped) {
+    hasSwapped = false;
 
-      arr[i] = result[0];
-      arr[i + 1] = result[1];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const result = swap(arr[i], arr[i + 1]);
 
-      // console.log('arr[i]:',arr[i],'-','arr[i+1]:',arr[i + 1]);
+        arr[i] = result[0];
+        arr[i + 1] = result[1];
+
+        hasSwapped = hasSwapped || true;
+      }
     }
   }
 
